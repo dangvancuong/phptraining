fcbf843bbb929a9fbbcfb4528c96c22ede7a7d55
Dang Van Cuongbcs <Dang Van Cuongbcs>
2016-07-07 20:00:53 +0700

    add new

---
+++ cuong.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Đặng Văn Cường</title>
+	<link rel="stylesheet" type="text/css" href="app.css">
+	
+</head>
+<body>
+	
+	<?php
+		$a = array('ms1' => 'cuong',
+					'ms2' => 'dang',
+					'ms3'=>'cuong'
+				);
+								
+		foreach ($a as $key => $value) {
+			echo "<p>".$value."</p>";
+		}
+		foreach ($a as $key => $value) {
+			echo "<p>".$value."</p>";
+		}
+	?>
+</body>
+</html>
